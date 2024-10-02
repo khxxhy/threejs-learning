04de266a15b2927405befd7b1a402c20d95ba92a
Manasvi Jaiswal <khxxhy@gmail.com>
2024-10-03 00:16:04 +0530

    renderloop

--- camera-and-render-loop/starter/src/script.js
+++ camera-and-render-loop/starter/src/script.js
@@ -1,31 +1,52 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 // initialize the scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // add objects to the scene
-const cubeGeometry = new THREE.BoxGeometry(1,1,1)
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"})
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
 
-const cubeMesh = new THREE.Mesh(
-  cubeGeometry,
-  cubeMaterial
-)
-scene.add(cubeMesh)
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cubeMesh);
 
-// initialize the camera
+//initialize the camera
 const camera = new THREE.PerspectiveCamera(
-  75, //field of view
+  65, //field of view
   window.innerWidth / window.innerHeight, //aspect
   0.1, //near
-  30) //far
-camera.position.z = 5
+  30 //far
+);
+
+/*const camera = new THREE.OrthographicCamera(
+  -1,
+  1,
+  1,
+  -1,
+  0.1,
+  50
+);*/
+
+camera.position.z = 5;
 
 // initialize the renderer
-const canvas = document.querySelector('canvas.threejs')
+const canvas = document.querySelector("canvas.threejs");
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
-})
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.render(scene, camera)
+  canvas: canvas,
+});
+//renderer.setSize(window.innerWidth, window.innerHeight);
+
+//instantiate the controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
+
+const renderloop = () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(renderloop);
+};
+
+renderloop();
